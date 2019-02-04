524bbd73179b0bf3d92a1d0c44198f217f3b759a
Ming Duan <11203duan@gmail.com>
2019-02-04 21:30:44 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+  new_collection = []
+  i = 0
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
 
-
+a = ["a"]
+my_collect(a) {
+  |each| each.split(" ").first
+}
